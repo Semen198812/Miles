20740715ae2d6adaef244b8a99181afd0d1cc66a
Семен Муллаяров <semen41ks@mail.ru>
2022-02-05 19:45:33 +0500

    initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 15623; // цена билета
+        int amount = 20; // количество рублей для одной бонусной мили
+        System.out.println("Количество бонусных миль:");
+        System.out.println((price / amount) + " бонусных миль");
+
+    }
+}
